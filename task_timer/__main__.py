f9e9ff0cf6b74787d35643e31d6acf4f467dd043
Tytus Woodburn <tytus.woodburn@student.cune.edu>
2025-02-11 20:03:39 -0600

    Resolved missing file header, capitalized global constants and redundant returns.

--- task_timer/__main__.py
+++ task_timer/__main__.py
@@ -1,17 +1,28 @@
+"""
+__main__.py
+
+Tytus Woodburn
+Email: tytus.woodburn@student.cune.edu
+Github: https://github.com/tywood01
+
+Purpose:
+This is a CLI tool that allows user to 
+create, delete, start, stop and view timed tasks.
+
+"""
 import click, csv
 from datetime import datetime, timedelta
 from pathlib import Path
 
 # Static Global Variables.
-filepath = Path(__file__).parent / "log.csv"
-cyan = "cyan"
-red = "red"
-green = "green"
+FILEPATH = Path(__file__).parent / "log.csv"
+RED = "red"
+GREEN = "green"
 
-def load_data_from_csv(filepath):
-    """Load data from filepath to dictionary."""
+def load_data_from_csv(FILEPATH):
+    """Load data from FILEPATH to dictionary."""
     data_dict = {}
-    with open(filepath, 'r', newline='') as csvfile:
+    with open(FILEPATH, 'r', newline='') as csvfile:
         reader = csv.reader(csvfile)
         for row in reader:
             task = row[0]
@@ -20,13 +31,12 @@ def load_data_from_csv(filepath):
 
     return data_dict
 
-def save_data_to_csv(data_dict, filepath):
-    """Save data from dictionary to filepath."""
-    with open(filepath, 'w', newline='') as csvfile:
+def save_data_to_csv(data_dict, FILEPATH):
+    """Save data from dictionary to FILEPATH."""
+    with open(FILEPATH, 'w', newline='') as csvfile:
         writer = csv.writer(csvfile)
         for task, timestamps in data_dict.items():
             writer.writerow([task] + timestamps)
-    return
 
 def sum_timestamps(timestamps):
     """Add up the total time given a sequence of timestamps formatted: [start, stop...]"""
@@ -56,7 +66,7 @@ def start(task):
     [TASKNAME] Starts a task timer for a given task.
     """
 
-    tasks = load_data_from_csv(filepath)
+    tasks = load_data_from_csv(FILEPATH)
 
     if task in tasks:
 
@@ -64,15 +74,14 @@ def start(task):
             tasks[task].append(datetime.now())
             
         else:
-            click.echo(click.style(f"ERROR:\nTask {task} is already started.", fg=red, bold=True))
+            click.echo(click.style(f"ERROR:\nTask {task} is already started.", fg=RED, bold=True))
             return
 
     else:
         tasks[task] = [datetime.now()]
 
-    click.echo(click.style(f"SUCCESS:\nTask {task} has been started.", fg=green, bold=True))
-    save_data_to_csv(tasks, filepath)
-    return
+    click.echo(click.style(f"SUCCESS:\nTask {task} has been started.", fg=GREEN, bold=True))
+    save_data_to_csv(tasks, FILEPATH)
 
 @main.command()
 @click.argument('task')
@@ -81,19 +90,18 @@ def stop(task):
     [TASKNAME] Stops a task timer for a given task.
     """
 
-    tasks = load_data_from_csv(filepath)
+    tasks = load_data_from_csv(FILEPATH)
 
     if task not in tasks or running(tasks[task]):
-        click.echo(click.style(f"ERROR:\nTask {task} hasn't been started.", fg=red, bold=True))
+        click.echo(click.style(f"ERROR:\nTask {task} hasn't been started.", fg=RED, bold=True))
         return
             
     else:
         tasks[task].append(datetime.now())
 
 
-    click.echo(click.style(f"SUCCESS:\nTask {task} has been stopped.", fg=green, bold=True))
-    save_data_to_csv(tasks, filepath)
-    return
+    click.echo(click.style(f"SUCCESS:\nTask {task} has been stopped.", fg=GREEN, bold=True))
+    save_data_to_csv(tasks, FILEPATH)
 
 
 @main.command()
@@ -103,7 +111,7 @@ def view(task, all):
     """
     [TASKNAME] Shows a summary of status and total time.
     """
-    tasks = load_data_from_csv(filepath)
+    tasks = load_data_from_csv(FILEPATH)
 
     if all or task == "":
         for task in tasks:
@@ -115,8 +123,7 @@ def view(task, all):
         print(f"Task {task} total time: {days} days, {hours} hours, {minutes} minutes, {seconds} seconds")
 
     else:
-        click.echo(click.style(f"ERROR:\nTask {task} hasn't been started.", fg=red, bold=True))
-    return
+        click.echo(click.style(f"ERROR:\nTask {task} hasn't been started.", fg=RED, bold=True))
 
 @main.command()
 @click.option("--all", is_flag=True, help="Deletes all entries for a task.")
@@ -125,30 +132,28 @@ def delete(task, all):
     """
     [TASKNAME] deletes the previous entry.
     """
-    tasks = load_data_from_csv(filepath)
+    tasks = load_data_from_csv(FILEPATH)
 
     if all:
-        if click.confirm(click.style(f"WARNING:\nThis will delete all entries for {task} are you sure?", fg=red, bold=True), abort=True):
+        if click.confirm(click.style(f"WARNING:\nThis will delete all entries for {task} are you sure?", fg=RED, bold=True), abort=True):
             tasks.pop(task, None) 
             
     else:
-        if click.confirm(click.style(f"WARNING:\nThis will delete the previous entry for {task} are you sure?", fg=red, bold=True), abort=True):
+        if click.confirm(click.style(f"WARNING:\nThis will delete the previous entry for {task} are you sure?", fg=RED, bold=True), abort=True):
             if running(tasks[task]):
                 tasks[task] = tasks[task][:-2]
             else:
                 tasks[task] = tasks[task][:-1]
 
-    save_data_to_csv(tasks, filepath)
+    save_data_to_csv(tasks, FILEPATH)
 
-    return
 
 @main.command()
 def edit():
     """
     Uses built-in text editor to manually edit timesheet.
     """
-    click.edit(filename=filepath)
-    return
+    click.edit(filename=FILEPATH)
 
 if __name__ == '__main__':
     main()
